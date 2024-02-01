0b998a0156bfd4d32a1715430b0fb3fd36bd4654
nialandin <nisanuraltuntas25@gmail.com>
2024-02-01 17:10:53 +0300

    deneme

--- src/main/java/pages/hotelpages/HotelPages.java
+++ src/main/java/pages/hotelpages/HotelPages.java
@@ -1,5 +1,21 @@
 package pages.hotelpages;
 
-public class HotelPages extends BasePage {
+import pages.BasePage;
 
+public class HotelPages extends BasePage {
+    private HotelConfirmationPage hotelConfirmationPage;
+    private HotelDetailsPage hotelDetailsPage;
+    private HotelPage hotelPage;
+    private HotelSelectionPage hotelSelectionPage;
+    private HotelsFinalStep hotelsFinalStep;
+    private HotelsYourDetailsPage hotelsYourDetailsPage;
+
+    public HotelPages() {
+        hotelConfirmationPage = new HotelConfirmationPage();
+        hotelDetailsPage = new HotelDetailsPage();
+        hotelPage = new HotelPage();
+        hotelSelectionPage = new HotelSelectionPage();
+        hotelsFinalStep = new HotelsFinalStep();
+        hotelsYourDetailsPage = new HotelsYourDetailsPage();
+    }
 }
