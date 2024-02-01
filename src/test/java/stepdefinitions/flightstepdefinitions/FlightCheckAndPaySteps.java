6c61255d0a0a724089a9190477718014f388b809
brkdgn <bburakddogan27@gmail.com>
2024-02-02 00:47:14 +0300

    Update

--- src/test/java/stepdefinitions/flightstepdefinitions/FlightCheckAndPaySteps.java
+++ src/test/java/stepdefinitions/flightstepdefinitions/FlightCheckAndPaySteps.java
@@ -30,6 +30,7 @@ public class FlightCheckAndPaySteps extends BaseStep {
 
     @When("Click on complete booking button")
     public void click_on_complete_booking_button() {
+        BrowserUtils.wait(1);
         PAGES.getFlightPages().getFlightCheckAndPayPage().clickOnCompleteBookingButton();
     }
 
