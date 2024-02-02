e9a3aaafbc9e83e0a2352efe3650b6c5357190ae
Berkan Eriş <berkan.eris.10@gmail.com>
2024-02-02 11:05:42 +0300

    The loggers added for test steps

--- src/test/java/stepdefinitions/flightstepdefinitions/FlightExtrasSteps.java
+++ src/test/java/stepdefinitions/flightstepdefinitions/FlightExtrasSteps.java
@@ -2,40 +2,79 @@ package stepdefinitions.flightstepdefinitions;
 
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import stepdefinitions.BaseStep;
 import utils.BrowserUtils;
 
 import static org.assertj.core.api.BDDAssertions.then;
 
 public class FlightExtrasSteps extends BaseStep {
+    private static final Logger LOGGER = LogManager.getLogger(FlightExtrasSteps.class);
     @And("Click on go to checkout  button")
     public void click_on_go_to_checkout_button() {
-        PAGES.getFlightPages().getFlightExtrasPage().clickOnGoToCheckOutButton();
+        try {
+            LOGGER.debug("Clicking on go to checkout button.");
+            PAGES.getFlightPages().getFlightExtrasPage().clickOnGoToCheckOutButton();
+            LOGGER.debug("Go to checkout button clicked successfully.");
+        } catch (Exception e) {
+            LOGGER.error("Error occurred while clicking on go to checkout button.", e);
+            throw e;
+        }
     }
 
     @And("Select {string} , {string} , {string}")
     public void select(String mealName, String extra1Name, String extra2Name) {
-        BrowserUtils.scrollDownWithJavaScript(0 , -600);
-        PAGES.getFlightPages().getFlightExtrasPage().selectMeal(mealName);
-        PAGES.getFlightPages().getFlightExtrasPage().selectExtra(extra1Name);
-        PAGES.getFlightPages().getFlightExtrasPage().selectExtra(extra2Name);
+        try {
+            LOGGER.debug("Selecting meal: {}, extra 1: {}, extra 2: {}.", mealName, extra1Name, extra2Name);
+            BrowserUtils.scrollDownWithJavaScript(0 , -600);
+            PAGES.getFlightPages().getFlightExtrasPage().selectMeal(mealName);
+            PAGES.getFlightPages().getFlightExtrasPage().selectExtra(extra1Name);
+            PAGES.getFlightPages().getFlightExtrasPage().selectExtra(extra2Name);
+            LOGGER.debug("Selection completed successfully.");
+        } catch (Exception e) {
+            LOGGER.error("Error occurred while making selections.", e);
+            throw e;
+        }
     }
 
     @Then("Verify that {string} , {string} , {string} is selected")
     public void verifyThatIsSelected(String mealName, String extra1Name, String extra2Name) {
-        then(PAGES.getFlightPages().getFlightExtrasPage().isMealSelected(mealName)).isTrue();
-        then(PAGES.getFlightPages().getFlightExtrasPage().isExtraSelected(extra1Name)).isTrue();
-        then(PAGES.getFlightPages().getFlightExtrasPage().isExtraSelected(extra2Name)).isTrue();
+        try {
+            LOGGER.debug("Verifying selections - meal: {}, extra 1: {}, extra 2: {}.", mealName, extra1Name, extra2Name);
+            then(PAGES.getFlightPages().getFlightExtrasPage().isMealSelected(mealName))
+                    .as("Meal selection check for " + mealName)
+                    .isTrue();
+
+            then(PAGES.getFlightPages().getFlightExtrasPage().isExtraSelected(extra1Name))
+                    .as("Extra 1 selection check for " + extra1Name)
+                    .isTrue();
+
+            then(PAGES.getFlightPages().getFlightExtrasPage().isExtraSelected(extra2Name))
+                    .as("Extra 2 selection check for " + extra2Name)
+                    .isTrue();
+            LOGGER.debug("Verification successful.");
+        } catch (Exception e) {
+            LOGGER.error("Error occurred while verifying selections.", e);
+            throw e;
+        }
     }
 
     @Then("Verify that total price calculation is correct after {string} , {string} , {string} selections")
     public void verifyThatTotalPriceCalculationIsCorrectAfterSelections(String mealName, String extra1Name, String extra2Name) {
-        String mealPrice = PAGES.getFlightPages().getFlightExtrasPage().getMealPrice(mealName);
-        String extra1Price = PAGES.getFlightPages().getFlightExtrasPage().getExtraPrice(extra1Name);
-        String extra2Price = PAGES.getFlightPages().getFlightExtrasPage().getExtraPrice(extra2Name);
-        double totalPrice = PAGES.getFlightPages().getFlightExtrasPage().getCalculatedTotalPrice();
-        double expectedTotalPrice = totalPrice + Double.parseDouble(mealPrice) + Double.parseDouble(extra1Price) + Double.parseDouble(extra2Price);
-        double actualTotalPrice = Double.parseDouble(PAGES.getFlightPages().getFlightExtrasPage().getTotalPrice());
-        then(actualTotalPrice).isEqualTo(expectedTotalPrice);
+        try {
+            LOGGER.debug("Verifying total price calculation after selections - meal: {}, extra 1: {}, extra 2: {}.", mealName, extra1Name, extra2Name);
+            String mealPrice = PAGES.getFlightPages().getFlightExtrasPage().getMealPrice(mealName);
+            String extra1Price = PAGES.getFlightPages().getFlightExtrasPage().getExtraPrice(extra1Name);
+            String extra2Price = PAGES.getFlightPages().getFlightExtrasPage().getExtraPrice(extra2Name);
+            double totalPrice = PAGES.getFlightPages().getFlightExtrasPage().getCalculatedTotalPrice();
+            double expectedTotalPrice = totalPrice + Double.parseDouble(mealPrice) + Double.parseDouble(extra1Price) + Double.parseDouble(extra2Price);
+            double actualTotalPrice = Double.parseDouble(PAGES.getFlightPages().getFlightExtrasPage().getTotalPrice());
+            then(actualTotalPrice).isEqualTo(expectedTotalPrice);
+            LOGGER.debug("Verification successful.");
+        } catch (Exception e) {
+            LOGGER.error("Error occurred while verifying total price calculation.", e);
+            throw e;
+        }
     }
 }
--- src/test/java/stepdefinitions/flightstepdefinitions/FlightSelectionSteps.java
+++ src/test/java/stepdefinitions/flightstepdefinitions/FlightSelectionSteps.java
@@ -22,23 +22,51 @@ public class FlightSelectionSteps extends BaseStep {
     public static List<String> returnFlightInfo;
     @And("Unselect {string} and Unselect {string} for cabin class")
     public void unselectAndUnselectForCabinClass(String classType1, String classType2) {
+        try {
+        LOGGER.debug("Unselecting cabin classes for round trip: {}, {}", classType1, classType2);
         PAGES.getFlightPages().getFlightSelectionPage().selectCabinClassForRoundTrip(classType1);
         PAGES.getFlightPages().getFlightSelectionPage().selectCabinClassForRoundTrip(classType2);
+        LOGGER.debug("Cabin classes unselected successfully.");
+        } catch (Exception e) {
+            LOGGER.error("Error occurred while unselecting cabin classes.", e);
+            throw e;
+        }
     }
 
     @And("Select {string} for airlines")
     public void selectForAirlines(String airlinesType) {
-        PAGES.getFlightPages().getFlightSelectionPage().selectAirlinesForRoundTrip(airlinesType);
+        try {
+            LOGGER.debug("Selecting airlines for round trip: {}", airlinesType);
+            PAGES.getFlightPages().getFlightSelectionPage().selectAirlinesForRoundTrip(airlinesType);
+            LOGGER.debug("Airlines selected successfully.");
+        } catch (Exception e) {
+            LOGGER.error("Error occurred while selecting airlines.", e);
+            throw e;
+        }
     }
 
     @And("Select {string} for duration \\(hours)")
     public void selectForDurationHours(String duration) {
-        PAGES.getFlightPages().getFlightSelectionPage().selectDurationHoursForRoundTrip(duration);
+        try {
+            LOGGER.debug("Selecting duration for round trip: {}", duration);
+            PAGES.getFlightPages().getFlightSelectionPage().selectDurationHoursForRoundTrip(duration);
+            LOGGER.debug("Duration selected successfully.");
+        } catch (Exception e) {
+            LOGGER.error("Error occurred while selecting duration.", e);
+            throw e;
+        }
     }
 
     @And("Click on search button")
     public void clickOnSearchButton() {
-        PAGES.getFlightPages().getFlightSelectionPage().clickOnSearchButton();
+        try {
+            LOGGER.debug("Clicking on the search button.");
+            PAGES.getFlightPages().getFlightSelectionPage().clickOnSearchButton();
+            LOGGER.debug("Search button clicked successfully.");
+        } catch (Exception e) {
+            LOGGER.error("Error occurred while clicking on the search button.", e);
+            throw e;
+        }
     }
     @And("Click on select ticket button for #{int} flight")
     public void clickOnSelectTicketButtonFor(int flightIndex) {
@@ -53,94 +81,221 @@ public class FlightSelectionSteps extends BaseStep {
 
     @And("Click on {string} tab")
     public void clickOnTab(String tabName) {
-        if(tabName.equals("Fastest")){
-            flightRanking = PAGES.getFlightPages().getFlightSelectionPage().getFlightHours();
-        }else {
-            flightRanking = PAGES.getFlightPages().getFlightSelectionPage().getFlightPrices();
+        try {
+            LOGGER.debug("Clicking on {} tab.", tabName);
+            if(tabName.equals("Fastest")){
+                flightRanking = PAGES.getFlightPages().getFlightSelectionPage().getFlightHours();
+            }else {
+                flightRanking = PAGES.getFlightPages().getFlightSelectionPage().getFlightPrices();
+            }
+            PAGES.getFlightPages().getFlightSelectionPage().clickOnTab(tabName);
+            LOGGER.debug("Tab {} clicked successfully.", tabName);
+        } catch (Exception e) {
+            LOGGER.error("Error occurred while clicking on {} tab.", tabName, e);
+            throw e;
         }
-        PAGES.getFlightPages().getFlightSelectionPage().clickOnTab(tabName);
     }
 
     @Then("Verify that the the cheapest flight is the first flight")
     public void verifyThatTheTheCheapestFlightIsTheFirstFlight() {
-        List<Double> flightPricesAfterCheapest = PAGES.getFlightPages().getFlightSelectionPage().getFlightPrices();
-        Collections.sort(flightRanking);
-        then(flightPricesAfterCheapest).isEqualTo(flightRanking);
+        try {
+            LOGGER.debug("Verifying that the cheapest flight is the first flight.");
+            List<Double> flightPricesAfterCheapest = PAGES.getFlightPages().getFlightSelectionPage().getFlightPrices();
+            Collections.sort(flightRanking);
+            then(flightPricesAfterCheapest).isEqualTo(flightRanking);
+            LOGGER.debug("Verification successful.");
+        } catch (Exception e) {
+            LOGGER.error("Error occurred while verifying the cheapest flight.", e);
+            throw e;
+        }
     }
 
     @Then("Verify that the the fastest flight is the first flight")
     public void verifyThatTheTheFastestFlightIsTheFirstFlight() {
-        List<Double> flightPricesAfterFastest = PAGES.getFlightPages().getFlightSelectionPage().getFlightHours();
-        Collections.sort(flightRanking);
-        then(flightPricesAfterFastest).isEqualTo(flightRanking);
+        try {
+            LOGGER.debug("Verifying that the fastest flight is the first flight.");
+            List<Double> flightPricesAfterFastest = PAGES.getFlightPages().getFlightSelectionPage().getFlightHours();
+            Collections.sort(flightRanking);
+            then(flightPricesAfterFastest).isEqualTo(flightRanking);
+            LOGGER.debug("Verification successful.");
+        } catch (Exception e) {
+            LOGGER.error("Error occurred while verifying the fastest flight.", e);
+            throw e;
+        }
     }
 
     @And("Click on select return ticket button for #{int} flight")
     public void clickOnSelectReturnTicketButtonForFlight(int flightIndex) {
-        PAGES.getFlightPages().getFlightSelectionPage().clickOnSelectTicket(flightIndex);
+        try {
+            LOGGER.info("Clicking on select return ticket button for flight #{}.", flightIndex);
+            PAGES.getFlightPages().getFlightSelectionPage().clickOnSelectTicket(flightIndex);
+            LOGGER.info("Select return ticket button clicked successfully.");
+        } catch (Exception e) {
+            LOGGER.error("Error occurred while clicking on select return ticket button for flight #{}.", flightIndex, e);
+            throw e;
+        }
     }
 
     @And("Select {string} from origin drop down")
     public void selectFromOriginDropDown(String originName) {
-        PAGES.getFlightPages().getFlightSelectionPage().selectFromOriginDropDown(originName);
+        try {
+            LOGGER.debug("Selecting {} from origin drop-down.", originName);
+            PAGES.getFlightPages().getFlightSelectionPage().selectFromOriginDropDown(originName);
+            LOGGER.debug("{} selected from origin drop-down.", originName);
+        } catch (Exception e) {
+            LOGGER.error("Error occurred while selecting {} from origin drop-down.", originName, e);
+            throw e;
+        }
     }
 
     @And("Select {string} from destination drop down")
     public void selectFromDestinationDropDown(String destinationName) {
-        PAGES.getFlightPages().getFlightSelectionPage().selectFromDestinationDropDown(destinationName);
+        try {
+            LOGGER.debug("Selecting {} from destination drop-down.", destinationName);
+            PAGES.getFlightPages().getFlightSelectionPage().selectFromDestinationDropDown(destinationName);
+            LOGGER.debug("{} selected from destination drop-down.", destinationName);
+        } catch (Exception e) {
+            LOGGER.error("Error occurred while selecting {} from destination drop-down.", destinationName, e);
+            throw e;
+        }
     }
 
     @And("Select {string} for Adult dropdown")
     public void selectForAdultDropdown(String adultNum) {
-        PAGES.getFlightPages().getFlightSelectionPage().selectFromAdultDropDownForRoundTrip(Integer.parseInt(adultNum));
+        try {
+            LOGGER.debug("Selecting {} for Adult dropdown.", adultNum);
+            PAGES.getFlightPages().getFlightSelectionPage().selectFromAdultDropDownForRoundTrip(Integer.parseInt(adultNum));
+            LOGGER.debug("{} selected for Adult dropdown.", adultNum);
+        } catch (Exception e) {
+            LOGGER.error("Error occurred while selecting {} for Adult dropdown.", adultNum, e);
+            throw e;
+        }
     }
 
     @And("Select {string} for Children dropdown")
     public void selectForChildrenDropdown(String childNum) {
-        PAGES.getFlightPages().getFlightSelectionPage().selectFromChildDropDownForRoundTrip(Integer.parseInt(childNum));
-        BrowserUtils.wait(3);
+        try {
+            LOGGER.debug("Selecting {} for Children dropdown.", childNum);
+            PAGES.getFlightPages().getFlightSelectionPage().selectFromChildDropDownForRoundTrip(Integer.parseInt(childNum));
+            BrowserUtils.wait(3);
+            LOGGER.debug("{} selected for Children dropdown.", childNum);
+        } catch (Exception e) {
+            LOGGER.error("Error occurred while selecting {} for Children dropdown.", childNum, e);
+            throw e;
+        }
     }
 
     @Then("Verify that {string} for origin, {string} for destination {string} for adult {string} for children dropdown is selected")
     public void verifyThatForOriginForDestinationForAdultForChildrenDropdown(String originName, String destinationName, String adultNum, String childrenNum) {
-        then(PAGES.getFlightPages().getFlightSelectionPage().isElementOnDropDownSelected(originName , "Origin")).isTrue();
-        then(PAGES.getFlightPages().getFlightSelectionPage().isElementOnDropDownSelected(destinationName , "Destination")).isTrue();
-        then(PAGES.getFlightPages().getFlightSelectionPage().isElementOnDropDownSelected(adultNum , "AdultForRoundTrip")).isTrue();
-        then(PAGES.getFlightPages().getFlightSelectionPage().isElementOnDropDownSelected(childrenNum , "ChildrenForRoundTrip")).isTrue();
+        try {
+            LOGGER.debug("Verifying that {} for origin, {} for destination, {} for adult, {} for children dropdown is selected.", originName, destinationName, adultNum, childrenNum);
+            then(PAGES.getFlightPages().getFlightSelectionPage().isElementOnDropDownSelected(originName, "Origin"))
+                    .as("Origin dropdown selection check")
+                    .isTrue();
+
+            then(PAGES.getFlightPages().getFlightSelectionPage().isElementOnDropDownSelected(destinationName, "Destination"))
+                    .as("Destination dropdown selection check")
+                    .isTrue();
+
+            then(PAGES.getFlightPages().getFlightSelectionPage().isElementOnDropDownSelected(adultNum, "AdultForRoundTrip"))
+                    .as("Adult dropdown selection check")
+                    .isTrue();
+
+            then(PAGES.getFlightPages().getFlightSelectionPage().isElementOnDropDownSelected(childrenNum, "ChildrenForRoundTrip"))
+                    .as("Children dropdown selection check")
+                    .isTrue();
+            LOGGER.debug("Verification successful.");
+        } catch (Exception e) {
+            LOGGER.error("Error occurred while verifying dropdown selections.", e);
+            throw e;
+        }
     }
 
     @And("Verify that {string} and {string} unselected for for cabin class, {string} selected for airlines , {string} selected for duration")
     public void verifyThatAndUnselectedForForCabinClassSelectedForAirlinesSelectedForDuration(String cabinClassName1, String cabinClassName2, String airlineName, String duration) {
-        then(PAGES.getFlightPages().getFlightSelectionPage().isElementSelectedOnList(cabinClassName1 , "CabinClassForRoundTrip")).isFalse();
-        then(PAGES.getFlightPages().getFlightSelectionPage().isElementSelectedOnList(cabinClassName2 , "CabinClassForRoundTrip")).isFalse();
-        then(PAGES.getFlightPages().getFlightSelectionPage().isElementSelectedOnList(airlineName , "AirlinesForRoundTrip")).isTrue();
-        then(PAGES.getFlightPages().getFlightSelectionPage().isElementSelectedOnList(duration , "DurationForRoundTrip")).isTrue();
+        try {
+            LOGGER.debug("Verifying that {} and {} unselected for for cabin class, {} selected for airlines, {} selected for duration.", cabinClassName1, cabinClassName2, airlineName, duration);
+            then(PAGES.getFlightPages().getFlightSelectionPage().isElementSelectedOnList(cabinClassName1, "CabinClassForRoundTrip"))
+                    .as("Cabin class 1 selection check")
+                    .isFalse();
+
+            then(PAGES.getFlightPages().getFlightSelectionPage().isElementSelectedOnList(cabinClassName2, "CabinClassForRoundTrip"))
+                    .as("Cabin class 2 selection check")
+                    .isFalse();
+
+            then(PAGES.getFlightPages().getFlightSelectionPage().isElementSelectedOnList(airlineName, "AirlinesForRoundTrip"))
+                    .as("Airlines selection check")
+                    .isTrue();
+
+            then(PAGES.getFlightPages().getFlightSelectionPage().isElementSelectedOnList(duration, "DurationForRoundTrip"))
+                    .as("Duration selection check")
+                    .isTrue();
+            LOGGER.debug("Verification successful.");
+        } catch (Exception e) {
+            LOGGER.error("Error occurred while verifying selections.", e);
+            throw e;
+        }
     }
 
     @And("Select departure date as {string}")
     public void selectDepartureDateAs(String departureDate) {
-        PAGES.getFlightPages().getFlightSelectionPage().selectDepartureDate(departureDate);
+        try {
+            LOGGER.debug("Selecting departure date as {}.", departureDate);
+            PAGES.getFlightPages().getFlightSelectionPage().selectDepartureDate(departureDate);
+            LOGGER.debug("Departure date selected successfully.");
+        } catch (Exception e) {
+            LOGGER.error("Error occurred while selecting departure date.", e);
+            throw e;
+        }
     }
 
     @And("Select return date as {string}")
     public void selectReturnDateAs(String returnDate) {
-        PAGES.getFlightPages().getFlightSelectionPage().selectReturnDate(returnDate);
+        try {
+            LOGGER.debug("Selecting return date as {}.", returnDate);
+            PAGES.getFlightPages().getFlightSelectionPage().selectReturnDate(returnDate);
+            LOGGER.debug("Return date selected successfully.");
+        } catch (Exception e) {
+            LOGGER.error("Error occurred while selecting return date.", e);
+            throw e;
+        }
     }
     @Then("Verify that {string} and {string} is selected")
     public void verifyThatAndIsSelected(String departureDate, String returnDate) {
-        then(PAGES.getFlightPages().getFlightSelectionPage().getSelectedDepartureDate()).isEqualTo(departureDate);
-        then(PAGES.getFlightPages().getFlightSelectionPage().getSelectedReturnDate()).isEqualTo(returnDate);
+        try {
+            LOGGER.debug("Verifying that {} and {} are selected.", departureDate, returnDate);
+            then(PAGES.getFlightPages().getFlightSelectionPage().getSelectedDepartureDate()).isEqualTo(departureDate);
+            then(PAGES.getFlightPages().getFlightSelectionPage().getSelectedReturnDate()).isEqualTo(returnDate);
+            LOGGER.debug("Verification successful.");
+        } catch (Exception e) {
+            LOGGER.error("Error occurred while verifying selected dates.", e);
+            throw e;
+        }
     }
 
     @And("Get the #{int} flight information and click on select ticket button")
     public void getTheFlightInformationAndClickOnSelectTicketButton(int flightIndex) {
-        departureFlightInfo = PAGES.getFlightPages().getFlightSelectionPage().getItemInformation(flightIndex);
-        PAGES.getFlightPages().getFlightSelectionPage().clickOnSelectTicket(flightIndex);
+        try {
+            LOGGER.debug("Getting the #{} flight information and clicking on select ticket button.", flightIndex);
+            departureFlightInfo = PAGES.getFlightPages().getFlightSelectionPage().getItemInformation(flightIndex);
+            PAGES.getFlightPages().getFlightSelectionPage().clickOnSelectTicket(flightIndex);
+            LOGGER.debug("Select ticket button clicked successfully.");
+        } catch (Exception e) {
+            LOGGER.error("Error occurred while getting flight information and clicking on select ticket button for flight #{}.", flightIndex, e);
+            throw e;
+        }
     }
 
     @And("Get the #{int} flight information and click on select return ticket button")
     public void getTheFlightInformationAndClickOnSelectReturnTicketButton(int flightIndex) {
-        returnFlightInfo = PAGES.getFlightPages().getFlightSelectionPage().getItemInformation(flightIndex);
-        PAGES.getFlightPages().getFlightSelectionPage().clickOnSelectTicket(flightIndex);
+        try {
+            LOGGER.debug("Getting the #{} flight information and clicking on select return ticket button.", flightIndex);
+            returnFlightInfo = PAGES.getFlightPages().getFlightSelectionPage().getItemInformation(flightIndex);
+            PAGES.getFlightPages().getFlightSelectionPage().clickOnSelectTicket(flightIndex);
+            LOGGER.debug("Select return ticket button clicked successfully.");
+        } catch (Exception e) {
+            LOGGER.error("Error occurred while getting flight information and clicking on select return ticket button for flight #{}.", flightIndex, e);
+            throw e;
+        }
     }
 }
