fc15ded7e9e9a1e6623f549514063451e6183a45
brkdgn <bburakddogan27@gmail.com>
2024-02-02 11:07:35 +0300

    update

--- src/test/java/stepdefinitions/flightstepdefinitions/FlightPassengerInformationSteps.java
+++ src/test/java/stepdefinitions/flightstepdefinitions/FlightPassengerInformationSteps.java
@@ -1,6 +1,4 @@
 package stepdefinitions.flightstepdefinitions;
-
-import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import org.apache.logging.log4j.LogManager;
@@ -8,78 +6,150 @@ import org.apache.logging.log4j.Logger;
 import org.assertj.core.api.Assertions;
 import stepdefinitions.BaseStep;
 import utils.BrowserUtils;
-import utils.Pages;
 
-import java.util.List;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.BDDAssertions.then;
 
 public class FlightPassengerInformationSteps extends BaseStep {
-    private static final Logger LOGGER = LogManager.getLogger(FlightHomeSteps.class);
+    private static final Logger LOGGER = LogManager.getLogger(FlightPassengerInformationSteps.class);
+
     @And("Fill {string} as contact email")
     public void fill_as_contact_email(String email)  {
-        PAGES.getFlightPages().getFlightPassengerInformationPage().fillContactEmail(email);
-    }
-    @And("Select {string} for country code dropdown and fill {string} as phone number")
-    public void select_for_country_code_dropdown_and_fill_as_phone_number(String countryCode, String phoneNumber) {
-        PAGES.getFlightPages().getFlightPassengerInformationPage().selectCountryCodeDropDown(countryCode);
-        PAGES.getFlightPages().getFlightPassengerInformationPage().fillPhoneNumber(phoneNumber);
-    }
-    @And("Fill in first name {string} last name {string} for passenger")
-    public void fillInFirstNameLastNameForPassenger(String firstName, String lastName) {
-        PAGES.getFlightPages().getFlightPassengerInformationPage().fillFirstName(firstName);
-        PAGES.getFlightPages().getFlightPassengerInformationPage().fillLastName(lastName);
-    }
-    @And("Select in the Gender as {string} from the gender dropdown for the 1st passenger")
-    public void select_in_the_gender_as_from_the_gender_dropdown_for_the_1st_passenger(String gender) {
-       PAGES.getFlightPages().getFlightPassengerInformationPage().selectGenderDropDown(gender);
-    }
-    @And("Select the year {string}, month {string}, day {string} as the date of birth")
-    public void select_the_year_month_day_as_the_date_of_birth(String year, String month, String day) {
-       PAGES.getFlightPages().getFlightPassengerInformationPage().selectYearDropDown(year);
-       PAGES.getFlightPages().getFlightPassengerInformationPage().selectMonthDropDown(month);
-       PAGES.getFlightPages().getFlightPassengerInformationPage().selectDayDropDown(day);
-    }
-    @And("Click on select extras button")
-    public void click_on_select_extras_button() {
-       PAGES.getFlightPages().getFlightPassengerInformationPage().clickOnSelectExtrasButton();
-    }
-
-
-    @Then("Verify that the {string} message is not displayed")
-    public void verifyThatTheMessageIsNotDisplayed(String expectedErrorMessage) {
-        String actualErrorMessage = PAGES.getFlightPages().getFlightPassengerInformationPage().findErrorMessage(expectedErrorMessage);
-        if (actualErrorMessage.equals(expectedErrorMessage)) {
-            Assertions.fail("Error Message: " + actualErrorMessage + " is displayed");
-        } else {
-            System.out.println("Test Valid");
+        try {
+            LOGGER.debug("Filling contact email as {}", email);
+            PAGES.getFlightPages().getFlightPassengerInformationPage().fillContactEmail(email);
+        } catch (Exception e) {
+            LOGGER.error("Error occurred while filling contact email: {}", e.getMessage());
+            throw e;
         }
     }
 
-   @And("Fill in {string} as the name, {string} as the surname, {string} as the gender, {string} as the year, {string} as the month, {string} as the day for the #{int} passenger")
+    @And("Select {string} for country code dropdown and fill {string} as phone number")
+    public void select_for_country_code_dropdown_and_fill_as_phone_number(String countryCode, String phoneNumber) {
+        try {
+            LOGGER.debug("Selecting country code: {} and filling phone number: {}", countryCode, phoneNumber);
+            PAGES.getFlightPages().getFlightPassengerInformationPage().selectCountryCodeDropDown(countryCode);
+            PAGES.getFlightPages().getFlightPassengerInformationPage().fillPhoneNumber(phoneNumber);
+        } catch (Exception e) {
+            LOGGER.error("Error occurred while selecting country code and filling phone number: {}", e.getMessage());
+            throw e;
+        }
+    }
+
+    @And("Fill in first name {string} last name {string} for passenger")
+    public void fillInFirstNameLastNameForPassenger(String firstName, String lastName) {
+        try {
+            LOGGER.debug("Filling passenger information - First Name: {}, Last Name: {}", firstName, lastName);
+            PAGES.getFlightPages().getFlightPassengerInformationPage().fillFirstName(firstName);
+            PAGES.getFlightPages().getFlightPassengerInformationPage().fillLastName(lastName);
+        } catch (Exception e) {
+            LOGGER.error("Error occurred while filling passenger information: {}", e.getMessage());
+            throw e;
+        }
+    }
+
+    @And("Select in the Gender as {string} from the gender dropdown for the 1st passenger")
+    public void select_in_the_gender_as_from_the_gender_dropdown_for_the_1st_passenger(String gender) {
+        try {
+            LOGGER.debug("Selecting gender for the 1st passenger: {}", gender);
+            PAGES.getFlightPages().getFlightPassengerInformationPage().selectGenderDropDown(gender);
+        } catch (Exception e) {
+            LOGGER.error("Error occurred while selecting gender: {}", e.getMessage());
+            throw e;
+        }
+    }
+
+    @And("Select the year {string}, month {string}, day {string} as the date of birth")
+    public void select_the_year_month_day_as_the_date_of_birth(String year, String month, String day) {
+        try {
+            LOGGER.debug("Selecting date of birth - Year: {}, Month: {}, Day: {}", year, month, day);
+            PAGES.getFlightPages().getFlightPassengerInformationPage().selectYearDropDown(year);
+            PAGES.getFlightPages().getFlightPassengerInformationPage().selectMonthDropDown(month);
+            PAGES.getFlightPages().getFlightPassengerInformationPage().selectDayDropDown(day);
+        } catch (Exception e) {
+            LOGGER.error("Error occurred while selecting date of birth: {}", e.getMessage());
+            throw e;
+        }
+    }
+
+    @And("Click on select extras button")
+    public void click_on_select_extras_button() {
+        try {
+            LOGGER.info("Clicking on select extras button");
+            PAGES.getFlightPages().getFlightPassengerInformationPage().clickOnSelectExtrasButton();
+        } catch (Exception e) {
+            LOGGER.error("Error occurred while clicking on select extras button: {}", e.getMessage());
+            throw e;
+        }
+    }
+
+    @Then("Verify that the {string} message is not displayed")
+    public void verifyThatTheMessageIsNotDisplayed(String expectedErrorMessage) {
+        try {
+            LOGGER.debug("Verifying that the '{}' message is not displayed", expectedErrorMessage);
+            String actualErrorMessage = PAGES.getFlightPages().getFlightPassengerInformationPage().findErrorMessage(expectedErrorMessage);
+            if (actualErrorMessage.equals(expectedErrorMessage)) {
+                Assertions.fail("Error Message: " + actualErrorMessage + " is displayed");
+            } else {
+                LOGGER.info("Test is valid");
+            }
+        } catch (Exception e) {
+            LOGGER.error("Error occurred while verifying error message: {}", e.getMessage());
+            throw e;
+        }
+    }
+
+    @And("Fill in {string} as the name, {string} as the surname, {string} as the gender, {string} as the year, {string} as the month, {string} as the day for the #{int} passenger")
     public void fillInAsTheNameAsTheSurnameAsTheGenderAsTheYearAsTheMonthAsTheDayForThePassenger(String firstName, String lastName, String gender, String year, String month, String day, int traveler) {
-        PAGES.getFlightPages().getFlightPassengerInformationPage().fillTravelerCard(traveler,firstName,lastName,gender,year,month,day);
+        try {
+            LOGGER.debug("Filling in traveler information - Name: {}, Surname: {}, Gender: {}, Year: {}, Month: {}, Day: {}, Passenger: #{}", firstName, lastName, gender, year, month, day, traveler);
+            PAGES.getFlightPages().getFlightPassengerInformationPage().fillTravelerCard(traveler,firstName,lastName,gender,year,month,day);
+        } catch (Exception e) {
+            LOGGER.error("Error occurred while filling traveler information: {}", e.getMessage());
+            throw e;
+        }
     }
 
     @And("Calculate total price")
     public double calculateTotalPrice() {
-        return PAGES.getFlightPages().getFlightPassengerInformationPage().getCalculatedTotalPrice();
-
+        try {
+            LOGGER.debug("Calculating total price");
+            return PAGES.getFlightPages().getFlightPassengerInformationPage().getCalculatedTotalPrice();
+        } catch (Exception e) {
+            LOGGER.error("Error occurred while calculating total price: {}", e.getMessage());
+            throw e;
+        }
     }
 
     @Then("Verify that total flight fare")
     public void verifyThatTotalFlightFare() {
-        BrowserUtils.wait(4);
-        String actualTotalPriceStr = String.valueOf(calculateTotalPrice());
-        String expectedTotalPrice=PAGES.getFlightPages().getFlightPassengerInformationPage().getTotalPrice();
-        Assertions.assertThat(actualTotalPriceStr).as("Actual and expected total prices should be equal")
-                .isEqualTo(expectedTotalPrice);
+        try {
+            LOGGER.info("Verifying total flight fare");
+            BrowserUtils.wait(2);
+            String actualTotalPriceStr = String.valueOf(calculateTotalPrice());
+            String expectedTotalPrice = PAGES.getFlightPages().getFlightPassengerInformationPage().getTotalPrice();
+
+            assertThat(actualTotalPriceStr)
+                    .as("Actual and expected total prices should be equal")
+                    .isEqualTo(expectedTotalPrice);
+        } catch (AssertionError e) {
+            LOGGER.error("Assertion error occurred while verifying total flight fare: {}", e.getMessage());
+            throw e;
+        } catch (Exception e) {
+            LOGGER.error("Error occurred while verifying total flight fare: {}", e.getMessage());
+            throw e;
+        }
     }
 
     @Then("Verify that the user is on passenger information page")
     public void verifyThatTheUserIsOnPassengerInformationPage() {
-        then(PAGES.getFlightPages().getFlightPassengerInformationPage().getHeaderText()).isEqualTo("Contact Details");
+        try {
+            LOGGER.debug("Verifying user is on passenger information page");
+            then(PAGES.getFlightPages().getFlightPassengerInformationPage().getHeaderText()).isEqualTo("Contact Details");
+        } catch (Exception e) {
+            LOGGER.error("Error occurred while verifying user is on passenger information page: {}", e.getMessage());
+            throw e;
+        }
     }
-
-
 }
